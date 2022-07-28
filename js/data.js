f73c7f7f78799c648c73489f6e7531b114b78922
Steven Mendoza <smendoza171@gmail.com>
2022-07-27 20:59:19 -0700

    first commit

---
+++ js/data.js
@@ -0,0 +1,45 @@
+const profileData = [
+  {
+    name: "Matilda",
+    avatar: "images/profile-pics/matilda.jpg",
+    age: 29,
+    bio: "Pop culture scholar. Unapologetic reader. Problem solver.",
+    hasBeenSwiped: false,
+    hasBeenLiked: false,
+  },
+  {
+    name: "Donovan",
+    avatar: "images/profile-pics/donovan.jpg",
+    age: 26,
+    bio: "Alcohol ninja.",
+    hasBeenSwiped: false,
+    hasBeenLiked: false,
+  },
+  {
+    name: "Jacky",
+    avatar: "images/profile-pics/jacky.jpg",
+    age: 30,
+    bio: "General travel nerd. Twitter enthusiast. Coffee junkie.",
+    hasBeenSwiped: false,
+    hasBeenLiked: false,
+  },
+  {
+    name: "Michael",
+    avatar: "images/profile-pics/michael.jpg",
+    age: 35,
+    bio: "Avid food fanatic. Extreme beer expert. Award-winning reader.",
+    hasBeenSwiped: false,
+    hasBeenLiked: false,
+  },
+  {
+    name: "Cindy",
+    avatar: "images/profile-pics/cindy.jpg",
+    age: 24,
+    bio: "Avid food fanatic. Extreme beer expert. Award-winning reader.",
+    hasBeenSwiped: false,
+    hasBeenLiked: false,
+  },
+];
+
+// Remember to export the data!
+export default profileData;
